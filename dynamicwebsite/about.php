e4e2855902ba6dd1d30f11e7e681485594ae52e7
Mandira Sahu <75577270+MandiraSahu@users.noreply.github.com>
2022-10-16 04:14:50 +0530

    Add files via upload

---
+++ dynamicwebsite/about.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">DarkAngels</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>DARK ANGELS !</h1>
+  <p>We, The Web Developers. Would love to help you in making your customize website.</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-12">
+        <img src="images/img3.jpg" class="img-fluid aboutimg">
+      </div>
+      <div class="col-lg-6 col-md-6 col-12">
+        <h2 class="display-4">We are Dark Angels.</h2>
+        <p class="py-0.8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum est sit amet mollis cursus. Phasellus vel sagittis mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vel suscipit sem. Morbi dignissim elit eget est lobortis, a iaculis ex mollis. Sed et lacus leo. Aenean malesuada ipsum dui, vitae porta ex bibendum sit amet.
+        </p>
+        <a href="about.php" class="btn btn-success"> Check more</a>
+      </div>
+    </div>
+</div>
+</section>
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@darkangelsProduction</p>
+</footer>
+
+
+</body>
+</html>
